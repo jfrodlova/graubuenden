10618e8428d0ec408675d2f6c2fd00942dcebb38
Jitka Frodlova <jitka.moutelikova@gmail.com>
2024-05-31 23:09:30 +0200

    wip

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,7 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 import { GiMountains } from "react-icons/gi";
 
@@ -9,12 +10,18 @@ function MyNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/"><GiMountains className="icon" size={50} /></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <GiMountains className="icon" size={50} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="./">About</Nav.Link>
-            <Nav.Link href="/weather">Weather</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/weather">
+              Weather
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
