f20e5fe9d231d05e090f5e3442a9e433ed303043
Jitka Frodlova <jitka.moutelikova@gmail.com>
2024-06-04 09:45:43 +0200

    fix routes, weather app styling

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import Weather from "./pages/Weather"
 import Error from "./pages/Error"
 import SharedLayout from "./pages/SharedLayout"
 
-
-
 const App = () => {
   return (
-    <BrowserRouter basename="/graubuenden">
+    <BrowserRouter basename="graubuenden">
       <Routes>
-        <Route element={ <SharedLayout /> } >
-          <Route path="/" element={ <Home /> } />
-          <Route path="/weather" element={ <Weather /> } />
-          <Route path="*" element={ <Error /> } />
+        <Route path="/" element={<SharedLayout />} >
+          <Route index element={<Home />} />
+          <Route path="weather" element={<Weather />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
-    
-  )
+  );
 }
 
 export default App
